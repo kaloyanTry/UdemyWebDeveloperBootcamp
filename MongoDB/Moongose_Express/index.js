d865194fe43e9c594d1555e8668cf989e3fc2590
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-09-17 21:51:56 +0300

    Update index.js

--- MongoDB/Moongose_Express/index.js
+++ MongoDB/Moongose_Express/index.js
@@ -14,16 +14,6 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
         console.log(err);
     })
 
-mongoose.connect('mongodb://localhost:27017/movieApp')
-    .then(() => {
-        console.log("CONNECTION OPEN!!!")
-    })
-    .catch(err => {
-        console.log("OH NO ERROR!!!!")
-        console.log(err)
-    });
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
