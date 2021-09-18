8959d385da8672d1ea68d0d6c7cb5df33b64c2cf
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-09-18 07:44:06 +0300

    Update index.js

--- MongoDB/Moongose_Express/index.js
+++ MongoDB/Moongose_Express/index.js
@@ -17,8 +17,9 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send('Woof!')
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products })
 })
 
 app.listen(3000, () => {
