45e6957fe89964e3a8681b1c8ab5e0e7ad6223ce
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-09-19 21:25:03 +0300

    Update product.js

--- MongoDB/Mongoose_Express_AppStages/models/product.js
+++ MongoDB/Mongoose_Express_AppStages/models/product.js
@@ -1,1 +1,21 @@
+const mongoose = require('mongoose');
 
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        emum: ['fruit', 'vegetable', 'dairy']
+    }
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
