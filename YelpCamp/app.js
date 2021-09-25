6a33bfcbafc46a1918708e8197c3c9a041f1feab
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-09-25 21:06:55 +0300

    Add files via upload

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    //old: useNewUserParser: true,
-    //old: useCreateIndex: true,
     useUnifiedTopology: true
 });
 
@@ -18,6 +17,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
