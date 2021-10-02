1b4daad11bae46ef863d2ad9b5e386b72fa59144
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-10-02 21:42:00 +0300

    Update index.js

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -18,12 +18,14 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
+    
     for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
+        const random1000 = Math.floor(Math.random() * 1000);        
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`
         });
+        
         await camp.save();
     }
 };
